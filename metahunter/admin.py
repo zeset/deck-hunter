9cfc21233ee1cacbabf3be7241a6cbfcbfd2a8c0
Nahuel B <nborda@alumnos.unc.edu.ar>
2019-11-10 19:11:45 -0300

    Metahunter, Deck, Card, Match, Player, InGameCard and InGameDeck models implemented

--- metahunter/admin.py
+++ metahunter/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Metahunter, Deck, Card
 
+admin.site.register(Metahunter)
+admin.site.register(Deck)
+admin.site.register(Card)
 # Register your models here.
--- metahunter/models.py
+++ metahunter/models.py
@@ -3,29 +3,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+#from metagame.models import Player, Match, InGameDeck, InGameCard
 
-INGAME = 'In Game'
-FINISHED = 'Finished'
+FRELJORD = 'Freljord'
+DEMACIA = 'Demacia'
+NOXUS = 'Noxus'
+IONIA = 'Ionia'
+LASISLASOSCURAS = 'Las Islas Oscuras'
+PILTOVER = 'Piltover'
+ZAUN = 'Zaun'
 
-GAMESTATES = {
-	(INGAME, 'In Game'),
-	(FINISHED, 'Finished'),
+ORIGINS = {
+	(FRELJORD, 'freljord'),
+	(DEMACIA, 'demacia'),
+	(NOXUS, 'noxus'),
+	(IONIA, 'ionia'),
+	(LASISLASOSCURAS, 'las_islas_oscuras'),
+	(PILTOVER, 'piltover'),
+	(ZAUN, 'zaun'),
 }
-
-
-class Player(models.Model):
-	username = models.CharField(max_length=20)
-	hp = models.IntegerField()
-	mp = models.IntegerField()
-
-
-class Match(models.Model):
-	state = models.CharField(max_length=20, choices=GAMESTATES)
-	winner = models.ManyToManyField(Player, related_name='wins')
-	opponents = models.ManyToManyField(Player, related_name='matches')
-
-	def logMatch():
-		pass
 
 
 class Metahunter(models.Model):
@@ -44,22 +40,6 @@ class Deck(models.Model):
 class Card(models.Model):
 	name = models.CharField(max_length=20)
 	code = models.CharField(max_length=100)
-	origin = models.CharField(max_length=50)
+	origin = models.CharField(max_length=50, choices=ORIGINS)
 	effectGenerated = models.BooleanField(default=False)
 	flavourText = models.CharField(max_length=200)
-
-
-class InGameDeck(models.Model):
-	ncards = models.IntegerField()
-
-
-class InGameCard(models.Model):
-	modified = models.BooleanField(default=False)
-	silenced = models.BooleanField(default=False)
-	mana_buff = models.IntegerField()
-	attack_buff = models.IntegerField()
-	health_buff = models.IntegerField()
-
-
-	def burnCard():
-		pass
